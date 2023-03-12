862c256810d48c45261cf32264db34811a763ba2
kristianrafuse <kristianrafuse@gmail.com>
2023-03-12 17:45:12 -0300

    Assignment 3 Complete
    Complete!

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,11 @@
 import os
 import csv
 
-# Set the file path for the budget data CSV file
+# Set the file path for the budget data CSV file. Not sure if I was suppose to use relative path or not...
+
 csvpath = os.path.join(r"PyBank\Resources\budget_data.csv")
 
-# Create empty lists to store the dates, months, profits, and total.
+# Create empty lists to store the dates, months, profits, and total which I'll use below.
 dates =[]
 months = []
 profits = []
@@ -14,7 +15,7 @@ total =[]
 # Open the CSV file and read the rows into the lists
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader) # Skip over/store the header row!
+    next(csvreader) # Skip over/store the header row! I think this is what "Successfully stores the header row (5 points)" meant in the "Requirements"
 
     for row in csvreader:
         dates.append(row[0])
@@ -24,33 +25,40 @@ with open(csvpath) as csvfile:
 months = len(dates)
 total = sum(profits)
 
-# Calculate the changes in profits from month to month and store them in a list
+# Calculate the changes in profits from month to month and store them in a list. Similar to VBA exercises!
 profit_loss = []
 for i in range(1, len(profits)):
     change = profits[i] - profits[i-1]
     profit_loss.append(change)
 
-# Calculate the total change in profits and the average change in profits from month to month. Took me a minute to realize the changes would be (month -1), not just month!
+# Calculate the total change in profits and the average change in profits from month to month. Took me a while to realize the changes would be (month -1), not just month!
 pl_total = sum(profit_loss)
 pl_avg = round(pl_total / (months-1), 2)
 
 # Find the greatest increase and greatest decrease in profits, along with their respective dates
+# Find the maximum value in the profit_loss list
 max_increase = max(profit_loss)
+
+# Find the index of the maximum value in the profit_loss list
 max_increase_index = profit_loss.index(max_increase)
+
+# Use the index of the maximum value to find the corresponding date in the dates list. +1 to the index because the first value in profit_loss represents the change from the second month to the first month.
 max_increase_date = dates[max_increase_index+1]
+
 max_increase_amount = max_increase
 
+#Same for mins!
 max_decrease = min(profit_loss)
 max_decrease_index = profit_loss.index(max_decrease)
 max_decrease_date = dates[max_decrease_index+1]
 max_decrease_amount = max_decrease
 
-# Print the results to the terminal
+# Print the results
 print("Total Months:",months)
 print("Total:$",total)
 print("Average Change:$",pl_avg)
 print("Greatest Increase in Profits:",max_increase_date,"($",max_increase_amount,")")
-print("Greatest Decrease in Profits:",max_decrease_date, "($",max_decrease_amount,")")
+print("Greatest Decrease in Profits:",max_decrease_date, "($",max_decrease_amount,")") #didn't know how to print as financial values or with "$"
 
 # Write the results to a text file -- Is there a quicker way to do this? 
 output_path = os.path.join(r"PyBank\Analysis\financial_analysis.txt")
@@ -63,4 +71,4 @@ with open(output_path, "w") as f:
     print("Greatest Increase in Profits:",max_increase_date,"($",max_increase_amount,")", file=f)
     print("Greatest Decrease in Profits:",max_decrease_date, "($",max_decrease_amount,")", file=f)
 
-# PyBank Assignment complete!
+print("PyBank Assignment complete!")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,13 @@
 import os
 import csv
 
-# set the path to the CSV file
+# set the path to the CSV file. Not sure if I was suppose to use relative path or not...
+
 csvpath = os.path.join(r"PyPoll\Resources\election_data.csv")
 
 # initialize some variables to hold the data we'll be collecting
 ballots = [] # list of ballot IDs
-candidates = set() # set of candidate names
+candidates = set() # set of candidate names. We didn't cover this in class (that I recall), but it seems to work well here.
 candidate_votes = {} # dictionary to store the number of votes each candidate receives
 total_votes = 0 # total number of votes cast
 
@@ -24,7 +25,7 @@ with open(csvpath) as csvfile:
         candidates.add(row[2])
         candidate = row[2]
 
-        # if we haven't seen this candidate before, add them to the dictionary of candidate votes
+        # if we haven't seen this candidate before, add them to the dictionary of candidate votes. Can see how this relates to previous VBA class assignments.
         if candidate not in candidate_votes:
             candidate_votes[candidate] = 0
         # increment the candidate's vote count
@@ -33,8 +34,13 @@ with open(csvpath) as csvfile:
         total_votes += 1
 
 #print out the results
+print("Election Results!")
+print("----------------------------")
+
 print(f'There were {total_votes} votes cast in the local election.')
-print(f"The following candidates received votes: *{candidates}")
+print("----------------------------")
+print("The following candidates received votes: " + ", ".join(candidates)) #use + ", ".join to print the set in a clearer fashion.
+print("----------------------------")
 
 # initialize a variable to keep track of the winning candidate
 winner = ''
@@ -44,8 +50,8 @@ winning_votes = 0
 # loop through each candidate and print out their percentage of the vote
 for candidate in candidate_votes:
     percentage = (candidate_votes[candidate] / total_votes) * 100
-    print(f'{candidate}: {percentage:.2f}%')
-    print(f'{candidate_votes[candidate]}')
+    print(f'{candidate} had {percentage:.2f}% of the votes, and {candidate_votes[candidate]} total votes.')
+    print("----------------------------")
 
     # check if this candidate has more votes than the current winner
     if candidate_votes[candidate] > winning_votes:
@@ -57,14 +63,20 @@ for candidate in candidate_votes:
 # print the winner of the election based on popular vote
 print(f'The winner of the election is {winner} with {winning_votes} and {winning_percentage:.2f}% of the popular vote')
 
-# Write the results to a text file
+# Write the results to a text file. Don't think this is any more efficient than the first assignment.
 output_path = os.path.join(r"PyPoll\Analysis\election_results.txt")
 with open(output_path, "w") as f:
-    print("Election Results",file=f)
-    print("----------------------------",file=f)
-    print(f'Total Votes:{total_votes}',file=f)
-    print(f"The following candidates received votes: {candidates}",file=f)
-    print("----------------------------",file=f)
-    print(f'Winner:{winner}',file=f)
+    f.write("Election Results\n")
+    f.write("-------------------------\n")
+    f.write(f"Total Votes: {total_votes}\n")
+    f.write("-------------------------\n")
+    for candidate in candidate_votes:
+        percentage = (candidate_votes[candidate] / total_votes) * 100
+        f.write(f"{candidate}: {percentage:.2f}% ({candidate_votes[candidate]})\n")
+    f.write("-------------------------\n")
+    f.write(f"Winner: {winner}\n")
+    f.write("-------------------------\n")
 
-print("PyPoll Assignment complete!")
+print("----------------------------")
+print("PyPoll Assignment complete! Module three complete! Woo hoo!")
+#may have overused the print("----------------------------"), but it makes it easier for me to read in the terminal! 
